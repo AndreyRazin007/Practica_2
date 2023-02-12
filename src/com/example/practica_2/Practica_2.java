5e11928baa82080aac1c30028c1a66185a366ed3
AndreyRazin <programingislife@gmail.com>
2023-02-13 01:02:32 +0500

    Added new Pracrica_2

--- src/com/example/practica_2/Practica_2.java
+++ src/com/example/practica_2/Practica_2.java
@@ -1,7 +1,17 @@
 package com.example.practica_2;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Practica_2 {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        ArrayList<Integer> source = new ArrayList<Integer>();
+        ArrayList<Integer> positive = new ArrayList<Integer>();
+        ArrayList<Integer> negative = new ArrayList<Integer>();
+        splitList(source, positive, negative);
+    }
+
+    public static void splitList(ArrayList<Integer> source, ArrayList<Integer> positive, ArrayList<Integer> negative) {
+
     }
 }
